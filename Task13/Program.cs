411a7c7864d1a0cdff66c1daa0faef0dc55dc65b
Denisova Olga <denisovazz77@gmail.com>
2023-09-27 18:08:27 +0300

    Task 13

---
+++ Task13/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enter number");
+int number = int.Parse(Console.ReadLine());
+
+if (number> 100)
+{
+    while (number>999)
+    {
+         number/=10;
+    }
+
+
+int result = number %10;
+System.Console.WriteLine(result);
+}
+
+else if (number<100)
+{
+    System.Console.WriteLine("Третьей цифры нет.");
+}
+ 
