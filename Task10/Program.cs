230c86ae78a595eceaf7a1af85d8199f61ec6578
Denisova Olga <denisovazz77@gmail.com>
2023-09-27 17:53:22 +0300

    Task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter number");
+int number = int.Parse(Console.ReadLine());
+int result = number/10%10;
+if (number>=100&&number<1000)
+{
+    System.Console.WriteLine(result);
+}
+else
+{
+    System.Console.WriteLine("Введено не трёхзначнле число!");
+}
