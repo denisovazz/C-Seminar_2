5b3a0c9fa9528dffbd416eb2f5cdcea629a823c7
Denisova Olga <denisovazz77@gmail.com>
2023-09-27 18:23:03 +0300

    Task 15

---
+++ Task15/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter number");
+int number = int.Parse(Console.ReadLine());
+ 
+if (number<1||number>7)
+{
+    System.Console.WriteLine("Такого дня недели нет.");
+}
+
+if (number==6||number==7)
+{
+    System.Console.WriteLine("Выходной");
+}
+
+if (number>0&&number<6)
+{
+    System.Console.WriteLine("Не выходной");
+}
